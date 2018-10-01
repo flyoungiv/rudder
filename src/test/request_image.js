57f63a59be7bcd1768f2f9329ee0fd1a60590ff9
Indiana Jones <flyoungiv@gmail.com>
2018-09-30 22:41:36 -0500

    testing for cover art auto-fill

---
+++ src/test/request_image.js
@@ -0,0 +1,21 @@
+
+var fs = require('fs');
+var request = require('request');
+
+request('https://cdn.thegamesdb.net/images/small/boxart/front/36615-1.jpg').pipe(fs.createWriteStream('bf1.jpg')).on('close', function(){
+    console.log('done');
+});
+
+//original code from stack overflow
+/*  var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+download('https://cdn.thegamesdb.net/images/small/boxart/front/36615-1.jpg', 'mom.png', function(){
+  console.log('done');
+}); */
