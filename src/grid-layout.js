082af7ef1ac620fe04f07dbd7b552c275fbf9000
Indiana Jones <flyoungiv@gmail.com>
2018-10-06 12:26:20 -0500

    cleaned up library

--- src/grid-layout.js
+++ src/grid-layout.js
@@ -1,5 +1,56 @@
 //let library = require('../data/library.json');
-let library = {"games":[{"id":60530,"game_title":"Overwatch","shortcut":"C:\\Program Files (x86)\\Battle.net\\Battle.net Launcher.exe","cover_art":"Overwatch-285x380.jpg"},{"id":1,"game_title":"Star Wars Battlefront II","shortcut":"D:\\Games\\Origin\\STAR WARS Battlefront II\\starwarsbattlefrontii.exe","cover_art":"2932265-sw_2017-apr-15.jpg"},{"id":1,"game_title":"Rocket League","shortcut":"D:\\Games\\Steam\\steamapps\\common\\rocketleague\\Binaries\\Win32\\RocketLeague.exe","cover_art":"2949245-box_rleague.png"},{"id":1,"game_title":"Portal","shortcut":"D:\\Games\\Steam\\steamapps\\common\\Portal\\hl2.exe","cover_art":"2525229-box_portal.png"},{"id":1,"game_title":"The Witcher 3 Wild Hunt","shortcut":"../src/test/launch_game.cmd","cover_art":"The Witcher 3_ Wild Hunt-285x380.jpg"},{"id":1,"game_title":"","shortcut":"","cover_art":""}]};
+let library = {
+	"games": [
+		{
+			"id": 60530,
+			"game_title": "Overwatch",
+			"shortcut": "C:\\Program Files (x86)\\Battle.net\\Battle.net Launcher.exe",
+			"cover_art": "Overwatch-285x380.jpg"
+		},
+		{
+			"id": 1,
+			"game_title": "Star Wars Battlefront II",
+			"shortcut": "D:\\Games\\Origin\\STAR WARS Battlefront II\\starwarsbattlefrontii.exe",
+			"cover_art": "2932265-sw_2017-apr-15.jpg"
+		},
+		{
+			"id": 1,
+			"game_title": "Rocket League",
+			"shortcut": "D:\\Games\\Steam\\steamapps\\common\\rocketleague\\Binaries\\Win32\\RocketLeague.exe",
+			"cover_art": "2949245-box_rleague.png"
+		},
+		{
+			"id": 1,
+			"game_title": "Portal",
+			"shortcut": "D:\\Games\\Steam\\steamapps\\common\\Portal\\hl2.exe",
+			"cover_art": "2525229-box_portal.png"
+		},
+		{
+			"id": 1,
+			"game_title": "The Witcher 3 Wild Hunt",
+			"shortcut": "../src/test/launch_game.cmd",
+			"cover_art": "The Witcher 3_ Wild Hunt-285x380.jpg"
+		},
+		{
+			"id": 1,
+			"game_title": "Ori and the Blind Forest",
+			"shortcut": "D:\\Games\\Steam\\steamapps\\common\\Ori\\ori.exe",
+			"cover_art": "ori.jpg"
+		},
+		{
+			"id": 1,
+			"game_title": "The Force Unleashed",
+			"shortcut": "D:\\Games\\Steam\\steamapps\\common\\Star Wars The Force Unleashed\\SWTFU Launcher.exe",
+			"cover_art": "2761846-tfu_cover_art.jpg"
+		},
+		{
+			"id": 1,
+			"game_title": "Half-Life 2",
+			"shortcut": "D:\\Games\\Steam\\steamapps\\common\\Half-Life 2\\hl2.exe",
+			"cover_art": "625502-hl2box.jpg"
+		}
+	]
+};
 let gameList = library.games;
 
 let html = '';
