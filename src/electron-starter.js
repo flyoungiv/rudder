5a1ebd56965827540afb2f9181786476fde73fe8
Indiana Jones <flyoungiv@gmail.com>
2019-01-16 11:24:56 -0600

    added npm foreman to help react start in electron window

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -19,7 +19,7 @@ function createWindow() {
     mainWindow.loadURL('http://localhost:3000');
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
